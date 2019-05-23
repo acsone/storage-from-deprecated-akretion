b61482a9deaabf7f02bec18a47253386bdd2e207
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-05-23 16:40:04 +0200

    [MIG] storage_image_category_pos: Backport from 10.0

--- storage_image_category_pos/__openerp__.py
+++ storage_image_category_pos/__openerp__.py
@@ -6,12 +6,12 @@
 {
     "name": "Storage Image Category POS",
     "summary": "Add image handling to product category and use it for POS",
-    "version": "10.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Storage",
     "website": "www.akretion.com",
     "author": " Akretion",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "depends": ["storage_image_product_pos", "pos_remove_pos_category"],
     "data": ["views/product_category.xml"],
 }
