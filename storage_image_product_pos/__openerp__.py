5c9850915b5c0d8c298845283854f9dba43a7040
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-05-23 16:38:14 +0200

    [MIG] storage_image_product_pos: Backport from 10.0

--- storage_image_product_pos/__openerp__.py
+++ storage_image_product_pos/__openerp__.py
@@ -6,12 +6,12 @@
 {
     "name": "Storage Image Product POS",
     "summary": "Link images to products and categories inside POS",
-    "version": "10.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Storage",
     "website": "www.akretion.com",
     "author": " Akretion",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "depends": ["storage_image_product", "point_of_sale"],
     "data": ["views/pos_product.xml"],
 }
